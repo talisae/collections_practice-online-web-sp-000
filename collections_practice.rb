cd19f9406b9711c4a619470150c57e2a259d8419
Talisa Erazo <talisaerazo@gmail.com>
2018-11-28 00:22:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,4 +33,14 @@ def sum_array(array)
     sum+=num
   end
   sum
-end 
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
