8e471706331f070469091e659d89699c3a337c58
Talisa Erazo <talisaerazo@gmail.com>
2018-11-28 00:05:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,11 @@ def sort_array_char_count(array)
   array.sort {|a, b| a.length <=> b.length}
 end
 
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
 def find_a(array)
   array.find_all {|word| word[0] == "a"}
 end
